198808eab775cd380a3db71dc4a6da34f45cfae3
klianglexpand <104772834+klianglexpand@users.noreply.github.com>
2022-11-13 17:16:17 -0500

    Fixed type

--- RestaurantService/queries.js
+++ RestaurantService/queries.js
@@ -6,7 +6,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'ResstaurantService',
+  database: 'RestaurantService',
   password: '123123',
   port: 5432,
 })
